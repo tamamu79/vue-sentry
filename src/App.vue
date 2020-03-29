653099181cf9223da586a137205d09ffde36250a
tamamu <masa.tmr79@gmail.com>
2020-03-29 09:22:05 +0900

    feat setting typescript and vue close #9 #10 #13

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  </div>
+</template>
+
+<script lang="ts">
+import HelloWorld from './components/HelloWorld.vue'
+import Vue from 'vue';
+
+export default Vue.extend({
+  name: 'app',
+  components: {
+    HelloWorld
+  }
+})
+</script>
+
+<style>
+</style>
